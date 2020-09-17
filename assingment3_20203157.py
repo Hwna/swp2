4cd4871dc81f8cb0f81c517c89950a8f597bfbf4
Hwna <ha07058@kookmin.ac.kr>
2020-09-17 09:22:02 +0900

    connect

---
+++ assingment3_20203157.py
@@ -0,0 +1,85 @@
+import pickle
+
+dbfilename = 'assingment3_20203157.dat'
+
+
+def readScoreDB():
+    try:
+        fH = open(dbfilename, 'rb')
+    except FileNotFoundError as e:
+        print("New DB: ", dbfilename)
+        return []
+
+    scdb = []
+    try:
+        scdb = pickle.load(fH)
+    except:
+        print("Empty DB: ", dbfilename)
+    else:
+        print("Open DB: ", dbfilename)
+    fH.close()
+    return scdb
+
+
+# write the data into person db
+def writeScoreDB(scdb):
+    fH = open(dbfilename, 'wb')
+    pickle.dump(scdb, fH)
+    fH.close()
+
+
+def doScoreDB(scdb):
+    while(True):
+        inputstr = (input("Score DB > "))
+        if inputstr == "":
+            continue
+        parse = inputstr.split(" ")
+
+        try:
+            if parse[0] == 'add':
+                record = {'Name': parse[1], 'Age': int(
+                    parse[2]), 'Score': int(parse[3])}
+                scdb += [record]
+
+            elif parse[0] == 'del':
+                for p in scdb[:]:
+                    if p['Name'] == parse[1]:
+                        scdb.remove(p)
+
+            elif parse[0] == 'show':
+                sortKey = 'Name' if len(parse) == 1 else parse[1]
+                showScoreDB(scdb, sortKey)
+
+            elif parse[0] == 'find':
+                for p in scdb:
+                    if p['Name'] == parse[1]:
+                        for attr in list(p.keys()):
+                            print(attr + "=" + str(p[attr]), end=" ")
+                        print()
+
+            elif parse[0] == 'inc':
+                name = parse[1]
+                amount = int(parse[2])
+                for p in scdb:
+                    if p['Name'] == name:
+                        p['Score'] += amount
+
+            elif parse[0] == 'quit':
+                break
+
+            else:
+                print("Invalid command: " + parse[0])
+        except IndexError:
+            pass
+
+
+def showScoreDB(scdb, keyname):
+    for p in sorted(scdb, key=lambda person: person[keyname]):
+        for attr in sorted(p):
+            print(attr + "=" + str(p[attr]), end=' ')
+        print()
+
+
+scoredb = readScoreDB()
+doScoreDB(scoredb)
+writeScoreDB(scoredb)
